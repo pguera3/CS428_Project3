568334f8e3aa49b25183db7d73c698712121d612
pguera3 <pguera3@uic.edu>
2020-11-28 19:56:56 -0600

    Project 3

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 15.0f;
+    public float rotationSpeed = 90;
+    public float force = 250f;
+    public GameObject cannon;
+    public GameObject bullet;
+
+    Rigidbody rb;
+    Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    public void shoot()
+    {
+
+        GameObject newBullet = GameObject.Instantiate(bullet, cannon.transform.position, cannon.transform.rotation) as GameObject;
+        newBullet.GetComponent<Rigidbody>().velocity += Vector3.up * 10;
+        newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * 1500);
+
+    }
+}
---
+++ Assets/Scripts/nearPerson.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nearPerson : MonoBehaviour
+{
+    public Animator anim;
+    private IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1.5f);
+        anim.SetBool("near", false);
+    }
+    // Update is called once per frame
+    public void play()
+    {
+        anim.SetBool("near", true);
+        coroutine = wait();
+        StartCoroutine(coroutine);
+    }
+}
